6273d1308e7f90e62e5d0072b1d484b7c46a49d9
Namees Albayati <nameesmohammed12@gmail.com>
2023-06-03 00:34:50 -0500

    add

--- controllers/fileroutes.js
+++ controllers/fileroutes.js
@@ -1,58 +1,69 @@
 const routes=require('express').Router();
 const { parseString } = require('xml2js');
-const nameRegex = /^[A-Za-z]+/;
-const numberRegex = /[0-9]+/;
-const userDataArr=[]
-const coopDataArr=[]
-const patterncorp = /CORP-\d{8}-\d{4}-\w{4}-\w{12}\//;
-
 const { Method, Environments } =require ('method-node');
 const method = new Method({
   apiKey: 'sk_phnDDCbykKzmaGgYLg3RR6Hi',
   env: Environments.dev,
 });
+const restOfFileArr=[]
+const nameRegex = /^[A-Za-z]+/;
+const numberRegex = /[0-9]+/;
+const userDataArr=[]
+const coopDataArr=[]
+const dunkinId=[]
+const mainIdDun=[]
+const formatDate=require('../helpers/helpersFun')
+const patterncorp = /CORP-\d{8}-\d{4}-\w{4}-\w{12}\//;
 
 
-const createEntANDcoop=async()=>{
-  const entity = await  method.entities.create({
-    type: 'individual',
-    individual: {
-      first_name: 'Kevin',
-      last_name: 'Doyle',
-      phone: '+16505555555',
-      email: 'kevin.doyle@gmail.com',
-      dob: '1997-03-18',
-    },
-    address: {
-      line1: '3300 N Interstate 35',
-      line2: null,
-      city: 'Austin',
-      state: 'TX',
-      zip: '78705',
-    },
-  });
-  const coop = await method.entities.create({
-    type: 'c_corporation',
-    corporation: {
-      name: 'Dunkin Donuts LLC',
-      dba: 'Dunkin Donuts',
-      ein: '32120240',
-      owners: [],
-    },
-    address: {
-      line1: '999 Hayes Lights',
-      line2: null,
-      city: 'Kerlukemouth',
-      state: 'TX',
-      zip: "78705",
-    },
-  });
+const creatDunkinMain=async()=>{
+
+    const dunkin = await method.entities.create({
+      type: 'c_corporation',
+      corporation: {
+        name: 'Namees Dunkin Donuts LLC',
+        dba: 'Dunkin Donuts',
+        ein: '32120240',
+        owners:[],
+      },
+      address: {
+        line1: '999 Hayes Lights',
+        line2: null,
+        city: 'Kerlukemouth',
+        state: 'CA',
+        zip: '94043',
+      },
+    });
+    return dunkin.id
+
+
 }
 
 
 
+
+const addingAccountsToDunkin=async(id)=>{
+console.log(id,dunkinId[0],' data dunkin accounts');
+dunkinId.forEach((ele)=>{
+  const source =  method.accounts.create({
+    holder_id: `${id}`,
+    ach: {
+      routing: `${ele[0]}`,
+      number: `${ele[1]}`,
+      type: 'checking',
+    },
+  }).then((sourceInfor)=>{
+   // console.log('accounts has created succesfully',sourceInfor)
+
+  })
+
+
+})
+
+}
+
+
 const processData=(data)=>{
-  //console.log('EMPID,BRANCHID',data[1].split('BRC-')[1].substring(0, 36))
  data.forEach(data1 => {
 
   let empId=`EMP-${data1.split('BRC-')[0]}`;
@@ -85,8 +96,11 @@ const processCoopFData=  (data)=>{
 const digits=element.slice(dunIndex+41);
 const routing=digits.substring(0,9);
 const numbRou=digits.substring(9,17)
+const rest=digits.substring(17)
+console.log('rest of file process',rest)
+restOfFileArr.push(rest)
 if(routing&&numbRou!==""){
-  createCoop(routing,numbRou)
+  dunkinId.push([routing,numbRou])
 
 
 }
@@ -97,19 +111,38 @@ if(routing&&numbRou!==""){
 
 
 
-routes.post('/file',(req,res)=>{
-  console.log('req.data',req.body)
+routes.post('/file',async(req,res)=>{
 if(req.body.data){
     parseString(req.body.data, (err, result) => {
         if (err) {
           console.error('Error parsing XML:', res.json({"message":"error while parsing data using xml2js"}));
         } else {
-
-
  const ParsedFileData=result.root.split('EMP-');
 
-processData(ParsedFileData)
 processCoopFData(ParsedFileData)
+
+processData(ParsedFileData)
+
+let data=  creatDunkinMain().then((dat)=>{
+  addingAccountsToDunkin(dat)
+
+  userDataArr.forEach((employeedata)=>{
+    method.entities.create({
+     type: 'individual',
+     individual: {
+       first_name: `${employeedata.first_name}`,
+       last_name: `${employeedata.last_name}`,
+       phone: "15121231111",
+       dob: `${formatDate(employeedata.dob)}`,
+     },
+     metadata:{"dunkinId":`${dat}`}
+   }).then((result)=>{
+    // console.log('entity emp has created',result)
+   })
+ })
+
+});
+
 
  res.status(300).json()
 
@@ -121,55 +154,23 @@ processCoopFData(ParsedFileData)
    
 
 })
+routes.get('/test',async(req,res)=>{
+  const merchants = await method.merchants.list();
+  const regex = /ins_\d+/gim;
+  restOfFileArr.forEach((element)=>{
+    if(element!==""||undefined||null){
+      const matches = element.match(regex);
+const loanAccountNu = matches[0].substring(matches[0].length - 8);
+const ins = matches[0].substr(0, matches[0].length - 8);
 
 
-const createCoop=async(routNu1,num2)=>{
-let creditData=[routNu1,num2]
-const credArr=[]
-const entId=[]
-const sourcACCArr=[]
-credArr.push(creditData)
-  const entity = await method.entities.create({
-    type: 'c_corporation',
-    individual:[{first_name:'branch1'},{first_name:'branch2'},{first_name:'branch3'},
-    {first_name:'branch4'},{first_name:'branch5'}],
-    corporation: {
-      name: 'Dunkin Donuts LLC',
-      dba: 'Dunkin Donuts',
-      ein: '32120240',
-      owners:[],
-    },
-    address: {
-      line1: '999 Hayes Lights',
-      line2: null,
-      city: 'Kerlukemouth',
-      state: 'CA',
-      zip: '94043',
-    },
-  });
-    entId.push(entity.id)
-    console.log(entity)
-    
-  //const combinedArray = credArr.map((subArray, index) => [...subArray, entId[index]]);
-/*
-const source = await method.accounts.create({
-  holder_id: `${combinedArray[0][2]}`,
-  ach: {
-    routing: `${combinedArray[0][0]}`,
-    number: `${combinedArray[0][1]}`,
-    type: 'checking',
-  },
-});
-sourcACCArr.push(source.id)
-*/
-
-
-
-
-
-  
 
 }
+  })
+ 
+
+}
+)
 
 
 module.exports=routes
