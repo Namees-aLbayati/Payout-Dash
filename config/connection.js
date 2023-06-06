322d3deb2cae8cc71d886aaa441c6b35157f8059
Namees Albayati <nameesalbayati@fb.com>
2023-06-06 16:20:18 -0500

    removing sequelize connection

--- config/connection.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('dunkin_db', 'root', '00000000', {
-    host: 'localhost',
-    dialect:'mysql',
-    port:3306
-  });
-  module.exports=sequelize
